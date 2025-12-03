aa488cb626fedc995028fb68eb7c3559e328d115
bhargav23032006 <bhargav23032006@gmail.com>
2025-12-03 15:15:14 +0530

    cron.js added

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import "dotenv/config"
 
-
+import job from './lib/cron.js';
 import authRoutes from './routes/authRoutes.js';
 import bookRoutes from './routes/bookRoutes.js';
 
@@ -11,6 +11,7 @@ import { connectDB } from './lib/db.js';
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+job.start();
 app.use(express.json());
 app.use(cors());
 
