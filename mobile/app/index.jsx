79c0ef665b8dda1917bfe6cb35a72ea879b13eeb
bhargav23032006 <bhargav23032006@gmail.com>
2025-12-03 14:42:35 +0530

    Add mobile app to repository

---
+++ mobile/app/index.jsx
@@ -0,0 +1,24 @@
+import { Link } from "expo-router";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Hello</Text>
+
+      <Link href="/(auth)/signup">Signup</Link>
+      <Link href="/(auth)">Login</Link>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+    justifyContent:'center',
+    alignItems:'center'
+  },
+  title:{
+    color:"blue",
+  }
+})
